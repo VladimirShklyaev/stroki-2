0346c1b00686d1b0b2f14f2fb37da3ddb02ab354
VladimirShklyaev <107680299+VladimirShklyaev@users.noreply.github.com>
2022-11-27 15:28:01 +0300

    Add files via upload

---
+++ 8-22/8-22.cpp
@@ -0,0 +1,46 @@
+﻿#include <string>
+#include <iostream>
+#include <windows.h>
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    setlocale(LC_ALL, "RUS");
+    using namespace std;
+    const int maxLength = 20;
+    char A[maxLength], B[maxLength];
+    std::cout << "A = ";
+    std::cin.getline(A, maxLength);
+    std::cout << "B = ";
+    std::cin.getline(B, maxLength);
+    int lengthB = strlen(B), k;
+    bool flag = true;
+    char* c;
+    for (int i = 0; i < lengthB; i++)
+    {
+        if (!strchr(A, B[i]))
+        {
+            flag = false;
+            break;
+        }
+        else
+        {
+            c = A;
+            k = 0;
+            for (char* p = A; *p; p++)
+            {
+                if (*p != B[i] || k)
+                {
+                    *c = *p;
+                    c++;
+                }
+                else k = 1;
+            }
+            *c = '\0';
+        }
+    }
+    if (flag)
+        std::cout << "Есть совпадение\n";
+    else std::cout << "Нет совпадения\n";
+    return 0;
+}
